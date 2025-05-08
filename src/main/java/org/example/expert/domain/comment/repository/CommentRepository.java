cd08ae0c92970f3c4487b29f4ec27276fdf5ad35
KSH-a <kimjune.net@gmail.com>
2025-05-08 21:38:40 +0900

    fix : "필수 Level 2 . 7. N+1"

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,16 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    //N + 1 문제가 생긴 상황
+    /*@Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);*/
+
+    // 1차 수정본
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
+
+    // 2차 수정본
+    /*@EntityGraph(attributePaths = "todoId")
+    List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);*/
 }
+
