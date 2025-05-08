a86fe66d0fc20c86b7900c35e172579999fd8808
KSH-a <kimjune.net@gmail.com>
2025-05-08 21:40:10 +0900

    fix : "필수 Level 2 . 8. QueryDSL"

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -14,8 +14,15 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
+    //QueryDSL
+    /*@Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
+            "WHERE t.id = :todoId")
+    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);*/
+
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
+
